6f299d33f1d4bd7aca76befa9c0eb1d012db8b56
omarelkeshen <oelkeshen5@gmail.com>
2024-08-24 10:13:50 +0300

    add supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://aquclkmlhnlivraoqwsf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxdWNsa21saG5saXZyYW9xd3NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzMTE1NjUsImV4cCI6MjAzOTg4NzU2NX0.OkPu3LJhRW__IbEHYGXE8TvZajcaVV0TYSE42tEGG4Q';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
