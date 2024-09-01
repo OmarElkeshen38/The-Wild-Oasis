aa50ded919af2b2ad8d8be7a1e2736566e67e080
omarelkeshen <oelkeshen5@gmail.com>
2024-09-01 12:04:17 +0300

    Setup react hook form

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,6 +15,11 @@ const Main = styled.main`
     padding: 4rem 4.8rem 6.4rem;
 `;
 
+const Container = styled.div`
+    max-width: 120rem;
+    margin: 0 auto;
+`;
+
 function AppLayout() {
     return (
         <StyledAppLayout>
@@ -22,7 +27,9 @@ function AppLayout() {
             <Sidebar />
 
             <Main>
-                <Outlet />
+                <Container>
+                    <Outlet />
+                </Container>
             </Main>
         </StyledAppLayout>
     )
