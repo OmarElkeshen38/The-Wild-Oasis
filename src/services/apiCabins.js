fb3c82e34af002e25a49d24d60d23e2adee5cca9
omarelkeshen <oelkeshen5@gmail.com>
2024-09-06 20:29:23 +0300

    create a new cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,23 +1,36 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-    const { data, error } = await supabase.from('cabins').select('*');
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
 
     if (error) {
         console.error(error);
-        throw new Error('Cabins could not be loaded');
-    }
-
-    return data;
+        throw new Error("Cabin could not be deleted");
+      }
+    
+      return data;
 }
 
 export async function deleteCabin(id) {
-    const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
-    if (error) {
-        console.error(error);
-        throw new Error('Cabin could not be deleted');
-    }
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
 
-    return data;
+  return data;
 }
